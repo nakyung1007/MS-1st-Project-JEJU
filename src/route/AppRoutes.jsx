6fe2c37aeda4fd8e06efd4ce46f5f93b6fd38f16
hoyoung.kim <the.hozero@gmail.com>
2024-11-05 10:04:45 +0900

    1. Indent Space 수정 2. 새로운 Design 추가

--- src/route/AppRoutes.jsx
+++ src/route/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Main from "../page/Main/Main.jsx";
 import ID from "../page/Input/ID.jsx";
 import Gender from "../page/Input/Gender.jsx";
@@ -12,26 +12,26 @@ import NotFound from "../page/error/NotFound.jsx";
 import Info from "../page/Main/Info.jsx";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      {/* Main Page */}
-      <Route path={"/"} element={<Main />} />
-      <Route path={"/info"} element={<Info />} />
+    return (
+        <Routes>
+            {/* Main Page */}
+            <Route path={"/"} element={<Main/>}/>
+            <Route path={"/info"} element={<Info/>}/>
 
-      {/* Input Page */}
-      <Route path={"/ID"} element={<ID />} />
-      <Route path={"/gender"} element={<Gender />} />
-      <Route path={"/age"} element={<Age />} />
-      <Route path={"/period"} element={<Period />} />
-      <Route path={"/season"} element={<Season />} />
-      <Route path={"/member"} element={<Member />} />
-      <Route path={"/sleep"} element={<Sleep />} />
-      <Route path={"/transportation"} element={<Transportation />} />
+            {/* Input Page */}
+            <Route path={"/id"} element={<ID/>}/>
+            <Route path={"/gender"} element={<Gender/>}/>
+            <Route path={"/age"} element={<Age/>}/>
+            <Route path={"/period"} element={<Period/>}/>
+            <Route path={"/season"} element={<Season/>}/>
+            <Route path={"/member"} element={<Member/>}/>
+            <Route path={"/sleep"} element={<Sleep/>}/>
+            <Route path={"/transportation"} element={<Transportation/>}/>
 
-      {/* Error Page */}
-      <Route path={"*"} element={<NotFound />} />
-    </Routes>
-  );
+            {/* Error Page */}
+            <Route path={"*"} element={<NotFound/>}/>
+        </Routes>
+    );
 };
 
 export default AppRoutes;
