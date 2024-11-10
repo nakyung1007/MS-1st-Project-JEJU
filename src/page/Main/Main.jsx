4dde4826e878520c48ca802d1961ca198d3ed5bb
hoyoung.kim <the.hozero@gmail.com>
2024-11-10 18:01:06 +0900

    1. 피드백 버튼 추가

--- src/page/Main/Main.jsx
+++ src/page/Main/Main.jsx
@@ -93,6 +93,24 @@ const Main = () => {
                     />
                 </div>
             </div>
+
+            <div className={"fixed right-4 bottom-3 z-50"}>
+                <button
+                    className={
+                        "bg-contain bg-center bg-no-repeat focus:outline-none z-50 " +
+                        "2xl:w-24 2xl:h-24 " +
+                        "xl:w-24 xl:h-24 " +
+                        "lg:w-20 lg:h-20 " +
+                        "md:w-16 md:h-16 " +
+                        "sm:w-12 sm:h-12 " +
+                        "w-8 h-8"
+                    }
+                    style={{
+                        backgroundImage: "url('/public/images/review.jpg')"
+                    }}
+                    onClick={() => window.open("https://forms.office.com/r/WuEfuqsNXe")}
+                />
+            </div>
         </div>
     );
 };
